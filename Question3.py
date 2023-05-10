40a7da4a16d1ab11c12cce3139d066c095ac620e
Murtaza1310 <132975705+Murtaza1310@users.noreply.github.com>
2023-05-10 11:56:30 +0530

    Add files via upload

---
+++ Question3.py
@@ -0,0 +1,20 @@
+# Explain the use of the following operators using an example
+
+#1 - /
+a = 4
+b = 4
+print(a/b)
+
+#2- %
+a = 5
+b = 10
+print(a%b)
+
+#3 - //
+a = 4
+b = 3
+print(a//b)
+
+#4 - **
+a = 4
+print(a**2)
