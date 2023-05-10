e3a898dda01bc273d90cf36577049dab9dc3f87c
Murtaza1310 <132975705+Murtaza1310@users.noreply.github.com>
2023-05-10 11:50:43 +0530

    Add files via upload

---
+++ Question2.py
@@ -0,0 +1,13 @@
+#Q2. Given are some following variables containing data .What will be the data type of the above given variable.
+
+var1 = ''
+print(type(var1))
+
+var2 = '[Ds, Ml, Python]'
+print(type(var2))
+
+var3 = ['Ds' , 'ML' , 'Python']
+print(type(var3))
+
+var4 = 1.
+print(type(var4))
