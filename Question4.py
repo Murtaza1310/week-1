cc460e07c224b8873e33569acf8ded658ded63c6
Murtaza1310 <132975705+Murtaza1310@users.noreply.github.com>
2023-05-10 12:03:24 +0530

    Add files via upload

---
+++ Question4.py
@@ -0,0 +1,6 @@
+#Q4. Create a list of length 10 of your choice containing multiple types of data. Using for loop print the 
+#element and its data type
+l = [10,20,40,'Naruto','Luffy',55.43,True,'hisoka',25.26,False]
+for i in l:
+    print(i)
+    print(type(i))
